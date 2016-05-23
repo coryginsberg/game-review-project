e7fb97a8a8c8211ea75bfa7fe0218b7b6a4b195d
Cory Ginsberg <cory@coryginsberg.com>
2016-05-22 21:13:05 -0500

    App now loads properly from last version. Also made the case of the name of the game no longer matter

--- app/review-detail.component.ts
+++ app/review-detail.component.ts
@@ -7,12 +7,11 @@ import {REVIEWS} from "./mock-reviews";
 import {Review} from "./review";
 import {MD_INPUT_DIRECTIVES} from "@angular2-material/input";
 import {MdButton} from "@angular2-material/button";
-import "unirest";
 
 @Component({
   selector: 'review-detail',
   template: `
-    <h2 class="md-display-1">{{title}}</h2>
+    <h2 class="md-display-1">{{game.title}}</h2>
     <div>
         <h3 class="md-headline">{{game.publisher}}</h3>
         <h1 class="md-headline">{{game.score}}</h1>
@@ -24,7 +23,6 @@ import "unirest";
     </li>
     <br>
     <button (click)="goBack()" type="submit" class="btn btn-default review-btn" md-raised-button color="warn">Search For Another Game</button>
-
   `,
   styleUrls: ['app/review-detail.component.css'],
   directives: [MD_INPUT_DIRECTIVES, MdButton, ROUTER_DIRECTIVES]
@@ -39,21 +37,13 @@ export class ReviewDetailComponent implements OnInit {
   }
 
   ngOnInit() {
-    var unirest = require('unirest');
-  
-    unirest.get("https://videogamesrating.p.mashape.com/get.php?count=1&game=the+elder+scrolls+skyrim")
-        .header("X-Mashape-Key", "3eVSH616qYmsh1v5ZbjRPu150bpyp10RGBDjsnWqAfBVERGFB8")
-        .header("Accept", "application/json")
-        .end(function (result) {
-          console.log(result.status, result.headers, result.body);
-        });
-  
+
     name = this.routeParams.get('title').replace(/%20/, " ");
     for (var game of REVIEWS) {
-      if (name == game.title) {
+      if (name.toUpperCase() == game.title.toUpperCase()) {
         this.title = this.routeParams.get('title').replace(/%20/, " ");
         this.game = game;
-      
+    
         for (var key in game.platforms) {
           let system = game.platforms[key];
           this.platforms.push(system);
--- systemjs.config.js
+++ systemjs.config.js
@@ -14,8 +14,7 @@
         'rxjs': 'node_modules/rxjs',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         '@angular': 'node_modules/@angular',
-        '@angular2-material': 'node_modules/@angular2-material',
-        'unirest': 'node_modules/unirest'
+        '@angular2-material': 'node_modules/@angular2-material'
     };
 
     // packages tells the System loader how to load when no filename and/or no extension
@@ -37,10 +36,6 @@
             format: 'cjs',
             defaultExtension: 'js',
             main: 'button.js'
-        },
-        unirest: {
-            defaultExtension: 'js',
-            main: 'index.js'
         }
     };
 
