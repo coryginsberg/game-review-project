c71a5d2a413e2ac6898cf59ba30eb140c932b6fe
Cory Ginsberg <cory@coryginsberg.com>
2016-05-28 01:46:56 -0500

    Did some things. Don't remember exactly...

--- app/giant_bomb.search.component.ts
+++ app/giant_bomb.search.component.ts
@@ -2,7 +2,7 @@
  * Created by Cory Ginsberg on 5/24/2016.
  */
 import {Injectable, Inject} from "@angular/core";
-import {Http} from "@angular/http";
+import {Jsonp, Response} from "@angular/http";
 // import {SearchResult} from "./review.component"
 
 let GIANTBOMB_API_KEY = '9e110c38f924128c200ce3bea458dd12fc4acc90';
@@ -10,27 +10,33 @@ let GIANTBOMB_API_URL = 'http://www.giantbomb.com/api/search/?';
 
 @Injectable()
 export class GiantBombService {
-    constructor(public http:Http, @Inject(GIANTBOMB_API_KEY) private apiKey, @Inject(GIANTBOMB_API_URL) private apiUrl) {
 
+    constructor(@Inject(GIANTBOMB_API_KEY) private apiKey,
+                @Inject(GIANTBOMB_API_URL) private apiUrl,
+                private jsonp:Jsonp) {
+
+        apiKey = GIANTBOMB_API_KEY;
+        apiUrl = GIANTBOMB_API_URL;
     }
 
-    // search(query: string): Observable<SearchResult[]> {
-    //     let params: string = [
-    //         `query=${query}`,
-    //         `api_key=${this.apiKey}`,
-    //         `field_list=aliases`,
-    //         `limit=5`,
-    //         `format=json`
-    //     ].join('&');
-    //     let queryUrl = `${this.apiUrl}?${params}`;
-    //
-    //     return this.http.get(queryUrl).map((response: Response) => {
-    //         return (<any>response.json().items.map(item => {
-    //             console.log("raw item", item); // uncomment if you want to debug
-    //             return new SearchResult({
-    //                 title: item.id.aliases
-    //             })
-    //         }))
-    //     })
-    // }
+    search(query:string) {
+        let params:string = [
+            `query=${query}`,
+            `api_key=${this.apiKey}`,
+            `field_list=aliases`,
+            `limit=5`,
+            `format=jsonp`,
+            `json_callback=JSONP_CALLBACK`
+        ].join('&');
+        let queryUrl = `${this.apiUrl}?${params}`;
+
+        return this.jsonp.get(queryUrl).map((response:Response) => {
+            return ((<any>response.json()).items.map(item => {
+                console.log("raw item", item); // uncomment if you want to debug
+                // return new SearchResult({
+                //     title: item.id.aliases
+                // })
+            }))
+        })
+    }
 }
--- app/review.component.ts
+++ app/review.component.ts
@@ -3,16 +3,11 @@
  *
  * Review Component of the App
  */
-import {Component, Inject, OnInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Router, ROUTER_DIRECTIVES} from "@angular/router-deprecated";
-import {REVIEWS} from "./mock-reviews";
 import {MD_INPUT_DIRECTIVES} from "@angular2-material/input";
 import {MdButton} from "@angular2-material/button";
 import {Http, Jsonp} from "@angular/http";
-
-
-let GIANTBOMB_API_KEY = '9e110c38f924128c200ce3bea458dd12fc4acc90';
-let GIANTBOMB_API_URL = 'http://www.giantbomb.com/api/search/?';
 
 @Component({
     selector: 'my-review',
@@ -24,7 +19,7 @@ let GIANTBOMB_API_URL = 'http://www.giantbomb.com/api/search/?';
         <div class="form-div">
             <form class="main-form">
                 <div class="game-div">
-                    <md-input #title (keyup.enter)="getTitle(title.value)" (blur)="getTitle(title.value)" type="text" dividerColor="warn" placeholder="Name of the Game" class="game-input"></md-input>
+                    <md-input #title  type="text" dividerColor="warn" placeholder="Name of the Game" class="game-input"></md-input>
                 </div>
     
                 <button (click)="makeRequest()" md-raised-button type="submit" color="warn" class="btn btn-default review-btn">Get Reviews</button>
@@ -43,8 +38,10 @@ export class ReviewComponent implements OnInit {
     loading:boolean;
     http:Http;
     jsonp:Jsonp;
+    private GIANTBOMB_API_KEY = '9e110c38f924128c200ce3bea458dd12fc4acc90';
+    private GIANTBOMB_API_URL = 'http://www.giantbomb.com/api/search/?';
 
-    constructor(private router:Router, http:Http, jsonp:Jsonp, @Inject(GIANTBOMB_API_KEY) private apiKey, @Inject(GIANTBOMB_API_URL) private apiUrl) {
+    constructor(private router:Router, http:Http, jsonp:Jsonp) {
         this.http = http;
         this.jsonp = jsonp;
     }
@@ -57,7 +54,6 @@ export class ReviewComponent implements OnInit {
         } else {
             console.log("Hello World");
         }
-
     }
 
     makeRequest(request:string) {
@@ -67,49 +63,26 @@ export class ReviewComponent implements OnInit {
     search(query:string) {
         let params:string = [
             `query=${query}`,
-            `api_key=${this.apiKey}`,
+            `api_key=${this.GIANTBOMB_API_KEY}`,
             `field_list=aliases`,
             `limit=5`,
             `format=jsonp`,
             `json_callback=JSONP_CALLBACK`
         ].join('&');
-        let queryUrl = `${this.apiUrl}?${params}`;
-
-        return this.jsonp.get(queryUrl).map(response => {
-            return (<any>response.items.map(item => {
+        let queryUrl = `${this.GIANTBOMB_API_URL}?${params}`;
+        this.loading = true;
+        this.jsonp.request(queryUrl).subscribe(response => {
+            (response.json().items.map(item => {
                 console.log("raw item", item); // uncomment if you want to debug
-                return new SearchResult({
-                    title: item.id.aliases
-                })
-            }))
-        })
+                // return new SearchResult({
+                //     title: item.id.aliases
+                // })
+            }));
+        });
     }
 
     ngOnInit() {
-        this.printToConsole();
-    }
 
-    printToConsole() {
-        console.log(REVIEWS[0].title);
-        console.log(REVIEWS[0].score);
-        console.log(REVIEWS[0].publisher);
-        console.log(REVIEWS[0].short_description);
-
-        for (var key in REVIEWS[0].platforms) {
-            var system = REVIEWS[0].platforms[key];
-            console.log('Key ' + key + ' has value : ' + system);
-        }
-
-        console.log(REVIEWS[0].platforms);
-        console.log(REVIEWS[0].thumb);
-    }
-}
-
-class SearchResult {
-    title:string;
-
-    constructor(obj?:any) {
-        this.title = obj && obj.title || null;
     }
 }
 
